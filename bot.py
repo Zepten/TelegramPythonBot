e9852fa4398ca527965ae6d2cf22c261a98ba161
Zepten <39930495+Zepten@users.noreply.github.com>
2021-03-25 23:23:28 +0300

    help command + some refactoring

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from logging import StringTemplateStyle
 import os
 from dotenv import load_dotenv
 import telebot
@@ -14,29 +15,40 @@ bot = telebot.TeleBot(os.environ.get('TOKEN'))
 # Словарь с настройками пользователей (вместо БД)
 settings_dict = {}
 
-def open_settings_file():
+# Обновление настроек пользователя
+def update_settings(user_id: int):
+	settings_dict.update({user_id: ['USD', False]})
+
+def set_currency(user_id: int, currency: str):
 	try:
-		with open('settings.json', 'r') as file:
-			global settings_dict
-			settings_dict = json.load(file)
-			print('Settings opened:')
-			print(settings_dict)
+		settings_dict[user_id][0] = currency
 	except:
-		update_settings_file()
+		update_settings(user_id)
+		settings_dict[user_id][0] = currency
 
-def update_settings_file():
-	with open('settings.json', 'w+') as file:
-		file.write(json.dumps(settings_dict))
-		print('Settings updated:')
-		print(settings_dict)
+def get_currency(user_id: int):
+	try:
+		return settings_dict[user_id][0]
+	except:
+		update_settings(user_id)
+		return settings_dict[user_id][0]
 
-open_settings_file()
+def toggle_newsletter(user_id: int):
+	try:
+		settings_dict[user_id][1] = not settings_dict[user_id][1]
+	except:
+		update_settings(user_id)
+		settings_dict[user_id][1] = not settings_dict[user_id][1]
+
+def get_newsletter(user_id: int):
+	return settings_dict[user_id][1]
 
 # Клавиатура
-markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
 markup.add(
 	telebot.types.KeyboardButton('Курс Bitcoin 📈'),
 	telebot.types.KeyboardButton('Выбрать валюту 💶'),
+	telebot.types.KeyboardButton('Включить расылку ✉️'),
 	telebot.types.KeyboardButton('Помощь 📎')
 )
 
@@ -53,17 +65,15 @@ def welcome(message):
 		"Привет, я умею отслеживать курс Bitcoin!\nНапиши /help, чтобы узнать, что я могу",
 		reply_markup=markup
 	)
-	settings_dict.update({str(message.from_user.id): {'cur': 'USD', 'news': 0}})
-	update_settings_file()
-	print(settings_dict)
 
 @bot.message_handler(commands=['bitcoin'])
 def bitcoin(message):
 	data = get_bitcoin_data()
-	cur = settings_dict[str(message.from_user.id)]['cur']
+	cur = get_currency(message.from_user.id)
 	bot.send_message(
 		message.chat.id,
-		f'Стоимость покупки Bitcoin в {cur}: {data[cur]["buy"]} {data[cur]["symbol"]}'
+		f'Стоимость покупки Bitcoin: *{data[cur]["buy"]} {data[cur]["symbol"]}*'.replace('.', ','),
+		parse_mode='MarkdownV2'
 	)
 
 @bot.message_handler(commands=['currency'])
@@ -81,12 +91,17 @@ def change_currency(message):
 		reply_markup=currency_markup
 	)
 
-@bot.message_handler(commands=['help'])
-def help(message):
-	bot.send_sticker(message.chat.id, open('stickers/help.webp', 'rb'))
-	bot.send_message(
-		message.chat.id, "Помощь\n"
-	)
+# Рассылка
+@bot.message_handler(commands=['newsletter'])
+def newsletter(message):
+	toggle_newsletter(message.chat.id)
+	if get_newsletter(message.chat.id):
+		bot.send_message(message.chat.id, 'Рассылка включена ✅')
+	else:
+		bot.send_message(message.chat.id, 'Рассылка отключена ⛔️')
+	while get_newsletter(message.chat.id):
+		bitcoin(message)
+		time.sleep(300) # Каждые 5 минут
 
 # Обработка нажатия на кнопки
 @bot.message_handler(content_types=['text'])
@@ -95,23 +110,28 @@ def send_text(message):
 		bitcoin(message)
 	elif message.text == 'Выбрать валюту 💶':
 		change_currency(message)
+	elif message.text == 'Включить расылку ✉️':
+		newsletter(message)
 	elif message.text == 'Помощь 📎':
 		help(message)
+
+@bot.message_handler(commands=['help'])
+def help(message):
+	bot.send_sticker(message.chat.id, open('stickers/help.webp', 'rb'))
+	bot.send_message(
+		message.chat.id, "Помощь\n"
+	)
 
 # Выбор валюты из Inline-клавиатуры
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-	settings_dict[str(call.from_user.id)]['cur'] = call.data
-	print(call.data)
+	set_currency(call.from_user.id, call.data)
 	bot.edit_message_text(
 		chat_id=call.message.chat.id,
 		message_id=call.message.message_id,
-		text=f'Выбрана валюта {settings_dict[str(call.from_user.id)]["cur"]}',
-		reply_markup=None
+		text=f'Выбрана валюта *{get_currency(call.from_user.id)}*',
+		reply_markup=None, parse_mode='MarkdownV2'
 	)
-
-	print(settings_dict)
-	update_settings_file()
 
 # Поллинг
 bot.polling(none_stop=True)
